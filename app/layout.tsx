ac5748c2d04a0f6b9328fb7e54164630598fab76
Achyut Paliwal <160041506+Achyut2009@users.noreply.github.com>
2026-01-06 00:29:28 +0300

    6th commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Yappachino",
-  description: "Developed by Achyut2009. Checout github his github repository for more info.",
+  description: "Developed by Achyut2009.",
 };
 
 export default function RootLayout({
