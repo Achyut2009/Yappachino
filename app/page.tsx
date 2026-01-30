ee3c47bc8847b310297b93acf811f3d984794cf7
Achyut Paliwal <achyutkpaliwal@gmail.com>
2026-01-30 12:21:01 +0300

    Co-authored-by: mywayarclinux <mywayarclinux@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home(){
   return(
     <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-black">
       <h1 className="text-2xl font-bold text-black dark:text-white">Welcome to MyWay!! Enjoy and have fun wiht my yaps!</h1>
-      <p className="mt-4 text-gray-600 dark:text-gray-300">Your personalized journey is gonna be here!!!</p>
+      <p className="mt-4 text-gray-600 dark:text-gray-300">Your personalized journey is gonna be here</p>
     </div>
   );
 }
